77786c2797811f540fc30b42198b76f4387faf7c
Ariane Wwelke <arianewelkep97@gmail.com>
2026-08-16 19:59:05 -0300

    feat(task-service): update task status on column change

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -9,13 +9,13 @@ import {
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
 import { ITask } from '../../interfaces/task.interface';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { TaskStatus } from '../../types/task-status';
 import { TaskStatusEnum } from '../../enums/task-status.enum';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCardComponent, CdkDropList, CdkDrag, AsyncPipe],
+  imports: [TaskCardComponent, CdkDropList, CdkDrag, AsyncPipe, JsonPipe],
   templateUrl: './task-list-section.component.html',
   styleUrl: './task-list-section.component.css'
 })
@@ -48,6 +48,12 @@ export class TaskListSectionComponent {
       default:
         throw Error('Coluno não identificada.');
     }
+
+    this._taskService.updateTaskStatus(
+      taskId,
+      taskCurrentStatus,
+      taskNextStatus
+    );
   }
 
   private moveCardToColumn(event: CdkDragDrop<ITask[]>) {
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,7 @@ import { ITask } from "../interfaces/task.interface";
 import { ITaskFormControls } from "../interfaces/task-form-controls.interface";
 import { generateUniqueIdWithTimestamp } from "../utils/generate-unique-id-timestamp";
 import { TaskStatusEnum } from "../enums/task-status.enum";
+import { TaskStatus } from "../types/task-status";
 
 @Injectable({
   providedIn: 'root'
@@ -39,7 +40,33 @@ export class TaskService {
     this.todoTasks$.next([...currentList, newTask]);
   }
 
-  carregarListaAtualDeTodos() {
-    console.log('Lista atual de TODOS: ', this.todoTasks$.value);
+  updateTaskStatus(taskId: string, taskCurrentStatus: TaskStatus, taskNextStatus: TaskStatus) {
+    const currentTaskList = this.getTaskListByStatus(taskCurrentStatus);
+    const nextTaskList = this.getTaskListByStatus(taskNextStatus);
+    const currentTask = currentTaskList.value.find((task) => task.id === taskId);
+
+    if (currentTask) {
+      // Atualiza o status da tarefa
+      currentTask.status = taskNextStatus;
+
+      // Removendo a terefa da lista atual
+      const currentTaskListWithoutTask = currentTaskList.value.filter
+         (task => task.id !== taskId,
+      );
+      currentTaskList.next([...currentTaskListWithoutTask]);
+
+      // Adicionando a tarefa na nova lista
+      nextTaskList.next([...nextTaskList.value, { ...currentTask }]);
+    }
+  }
+
+  private getTaskListByStatus(taskStatus: TaskStatus) {
+    const taskListObj = {
+      [TaskStatusEnum.TODO]: this.todoTasks$,
+      [TaskStatusEnum.DOING]: this.doingTasks$,
+      [TaskStatusEnum.DONE]: this.doneTasks$,
+    };
+
+    return taskListObj[taskStatus];
   }
 }
